1ac238c4fdd30c5d3d5dd2e7d9311ee9142f51d9
a916856595 <hcj147@vip.qq.com>
2022-03-09 23:49:14 +0800

    refactor: prepare to use middleware

--- service/index.ts
+++ service/index.ts
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+
+app.use('/api', (request: any, response: any, next: any) => {
+  response.send('api');
+  next();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Service listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 import './reset.scss';
-
-console.log(123);
